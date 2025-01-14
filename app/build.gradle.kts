b3d8c9b484bb51d1eb23f68016755647b822eef3
kajak <gegenteilvongegenteil@gmail.com>
2025-01-14 13:15:37 +0100

    Init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.betterbe"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.betterbe"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
