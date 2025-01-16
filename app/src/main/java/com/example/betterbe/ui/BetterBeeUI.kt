b427801732b91ee9831175a78dafd6d9a40b2e73
kajak <gegenteilvongegenteil@gmail.com>
2025-01-16 16:42:38 +0100

    Navbar only on certain screens

--- app/src/main/java/com/example/betterbe/ui/BetterBeeUI.kt
+++ app/src/main/java/com/example/betterbe/ui/BetterBeeUI.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.betterbe.ui.add.AddHabitView
@@ -30,9 +32,18 @@ fun HabitApp(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController()
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val routesWithNavBar = listOf(Routes.Home.route, Routes.Manager.route)
+
     Scaffold(
         modifier.fillMaxSize(),
-        bottomBar = { BottomNavBar(navController) }
+        bottomBar = {
+            if (currentRoute in routesWithNavBar) {
+            BottomNavBar(navController)
+        }
+        }
     ) { innerPadding ->
 
         NavHost(
