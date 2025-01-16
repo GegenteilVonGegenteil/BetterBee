3a8cda795c8554776cbd2e8fa7949a9abae50291
kajak <gegenteilvongegenteil@gmail.com>
2025-01-16 16:52:14 +0100

    View updates on change

--- app/src/main/java/com/example/betterbe/ui/home/HomeView.kt
+++ app/src/main/java/com/example/betterbe/ui/home/HomeView.kt
@@ -9,13 +9,13 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.betterbe.R
 import com.example.betterbe.ui.AppViewModelProvider
@@ -29,9 +29,10 @@ fun HomeView(
     onHabitClick: (Int) -> Unit
 ) {
 
-    val habitsWithStatus by homeViewModel.habitsWithCompletionStatus.observeAsState(emptyList())
+    val habitsWithStatus by homeViewModel.habitsWithCompletionStatus.collectAsStateWithLifecycle()
     val completedCount = homeViewModel.getCompletedHabitsCount(habitsWithStatus)
     val totalCount = habitsWithStatus.size
+
 
     Column(modifier = Modifier.fillMaxSize()){
 
--- app/src/main/java/com/example/betterbe/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/betterbe/ui/home/HomeViewModel.kt
@@ -1,30 +1,41 @@
 package com.example.betterbe.ui.home
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.betterbe.data.Habit
 import com.example.betterbe.data.HabitRepository
 import com.example.betterbe.data.db.CompletionStatusEntity
 import com.example.betterbe.data.db.HabitEntity
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class HomeViewModel(private val repository: HabitRepository) : ViewModel() {
-    fun changeCompletionStatus(habit: HabitEntity) {
+
+    private val _habitsWithCompletionStatus = MutableStateFlow<List<Pair<HabitEntity, CompletionStatusEntity?>>>(emptyList())
+    val habitsWithCompletionStatus: StateFlow<List<Pair<HabitEntity, CompletionStatusEntity?>>>
+        get() = _habitsWithCompletionStatus
+
+    init {
+        loadHabits()
+    }
+
+    private fun loadHabits() {
         viewModelScope.launch {
-            repository.changeCheckedStatus(habit._id)
+            repository.getAllHabits().collect { habits ->
+                val habitsWithStatus = habits.map { habit ->
+                    habit to repository.getLastCompletionStatus(habit._id)
+                }
+                _habitsWithCompletionStatus.value = habitsWithStatus
+            }
         }
     }
 
-    val habitsWithCompletionStatus: LiveData<List<Pair<HabitEntity, CompletionStatusEntity?>>> = repository.getAllHabits()
-        .map { habits ->
-            habits.map { habit ->
-                habit to repository.getLastCompletionStatus(habit._id)
-            }
+    fun changeCompletionStatus(habit: HabitEntity) {
+        viewModelScope.launch {
+            repository.changeCheckedStatus(habit._id)
+            loadHabits()
         }
-        .asLiveData()
+    }
 
     fun getCompletedHabitsCount(habitsWithStatus: List<Pair<HabitEntity, CompletionStatusEntity?>>): Int {
         return habitsWithStatus.count { it.second?.completed == true }
