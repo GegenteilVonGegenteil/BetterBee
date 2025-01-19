a05a17b495fcf64c945c774ca485cff99fb6dc70
kajak <gegenteilvongegenteil@gmail.com>
2025-01-20 00:47:47 +0100

    Worker should work, need to add page refresh, not sure why it didnt work on phone

--- app/src/main/java/com/example/betterbe/workManager/refreshScheduler.kt
+++ app/src/main/java/com/example/betterbe/workManager/refreshScheduler.kt
@@ -9,18 +9,6 @@ import java.util.Calendar
 import java.util.concurrent.TimeUnit
 
 
-fun scheduleTestCompletionStatusWorker(context: Context) {
-    val workRequest = PeriodicWorkRequestBuilder<CreateCompletionStatusWorker>(2, TimeUnit.MINUTES)
-        .setInitialDelay(2, TimeUnit.MINUTES) // Initial delay of 5 minutes
-        .build()
-
-    WorkManager.getInstance(context).enqueueUniquePeriodicWork(
-        "CreateCompletionStatusWorker",
-        ExistingPeriodicWorkPolicy.REPLACE,
-        workRequest
-    )
-}
-
 fun scheduleDailyCompletionStatusWorker(context: Context) {
     val workRequest = PeriodicWorkRequestBuilder<CreateCompletionStatusWorker>(1, TimeUnit.DAYS)
         .setInitialDelay(calculateInitialDelay(), TimeUnit.MILLISECONDS)
@@ -38,10 +26,12 @@ fun calculateInitialDelay(): Long {
     val currentTime = Calendar.getInstance()
     val midnightTime = Calendar.getInstance().apply {
         set(Calendar.HOUR_OF_DAY, 0)
-        set(Calendar.MINUTE, 21)
+        set(Calendar.MINUTE, 0)
         set(Calendar.SECOND, 0)
         set(Calendar.MILLISECOND, 0)
-        add(Calendar.DAY_OF_MONTH, 1)
+        if (before(currentTime)) {
+            add(Calendar.DAY_OF_MONTH, 1)
+        }
     }
     return midnightTime.timeInMillis - currentTime.timeInMillis
 }
