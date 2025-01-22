e82bd0a93c08476d01c1fa6488e6b2cefc1d82da
kajak <gegenteilvongegenteil@gmail.com>
2025-01-22 16:46:18 +0100

    Better Calender view

--- app/src/main/java/com/example/betterbe/ui/detail/DetailView.kt
+++ app/src/main/java/com/example/betterbe/ui/detail/DetailView.kt
@@ -5,19 +5,16 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -35,6 +32,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.betterbe.R
+import com.example.betterbe.data.CompletionStatus
 import com.example.betterbe.ui.AppViewModelProvider
 import com.kizitonwose.calendar.compose.HorizontalCalendar
 import com.kizitonwose.calendar.compose.rememberCalendarState
@@ -42,7 +40,6 @@ import com.kizitonwose.calendar.core.CalendarDay
 import com.kizitonwose.calendar.core.DayPosition
 import com.kizitonwose.calendar.core.daysOfWeek
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
-import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.TextStyle
@@ -55,7 +52,20 @@ fun DetailView(
 ) {
     val state by detailViewModel.habitDetailUIState.collectAsStateWithLifecycle()
     val completionStatuses by detailViewModel.getCompletionStatusesForHabit(state.habit.id).collectAsStateWithLifecycle(emptyList())
+    val existingDates = completionStatuses.map {it.date}
     val markedDates = completionStatuses.filter { it.completed }.map { it.date }
+
+    val habitColor = when (state.habit.color) {
+        "red" -> colorResource(R.color.red_light)
+        "orange" -> colorResource(R.color.orange_light)
+        "yellow" -> colorResource(R.color.yellow_light)
+        "green" -> colorResource(R.color.green_light)
+        "blue" -> colorResource(R.color.blue_light)
+        "violet" -> colorResource(R.color.violet_light)
+        else -> {
+            colorResource(R.color.yellow_light)
+        }
+    }
 
     FloatingActionButton(
         onClick = {  navController.popBackStack() },
@@ -91,7 +101,9 @@ fun DetailView(
         ){
 
             CalendarView(
-                markedDates = markedDates
+                markedDates = markedDates,
+                habitColor,
+                existingDates
             )
         }
 
@@ -99,11 +111,11 @@ fun DetailView(
 }
 
 @Composable
-fun CalendarView(markedDates: List<LocalDate>) {
+fun CalendarView(markedDates: List<LocalDate>, habitColor: Color, existingDates: List<LocalDate>) {
     val currentMonth = remember { YearMonth.now() }
-    val startMonth = remember { currentMonth.minusMonths(100) } // Adjust as needed
-    val endMonth = remember { currentMonth.plusMonths(100) } // Adjust as needed
-    val firstDayOfWeek = remember { firstDayOfWeekFromLocale() } // Available from the library
+    val startMonth = remember { currentMonth.minusMonths(100) }
+    val endMonth = remember { currentMonth.plusMonths(100) }
+    val firstDayOfWeek = remember { firstDayOfWeekFromLocale() }
     val daysOfWeek = daysOfWeek()
 
     val state = rememberCalendarState(
@@ -131,7 +143,7 @@ fun CalendarView(markedDates: List<LocalDate>) {
                     textAlign = TextAlign.Center,
                     text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
                     style = androidx.compose.ui.text.TextStyle(
-                        color = colorResource(R.color.mustard_400)
+                        color = habitColor
                     )
                 )
             }
@@ -140,32 +152,39 @@ fun CalendarView(markedDates: List<LocalDate>) {
         HorizontalCalendar(
             state = state,
             dayContent = { day ->
-                Day(day, markedDates)
+                Day(day, markedDates, habitColor, existingDates)
             },
         )
     }
 }
 
 @Composable
-fun Day(day: CalendarDay, markedDates: List<LocalDate>) {
+fun Day(day: CalendarDay, markedDates: List<LocalDate>, habitColor: Color, existingDates: List<LocalDate>) {
     val isMarked = day.date in markedDates
     val isInMonth = day.position == DayPosition.MonthDate
+    val earliestDate = existingDates.minOrNull() ?: LocalDate.now()
+    val isBeforeEarliestDate = day.date.isBefore(earliestDate)
+    val isToday = day.date.isEqual(LocalDate.now())
 
     Box(
         modifier = Modifier
             .aspectRatio(1f)
             .padding(4.dp)
             .background(
-                color = if (isMarked) colorResource(R.color.bronco_50) else Color.Transparent,
+                color = if (isMarked) habitColor else Color.Transparent,
                 shape = if (isMarked) CircleShape else RectangleShape
-            ),
+            )
+            .border(
+                width = 2.dp,
+                color = if (isToday) colorResource(R.color.bronco_50) else Color.Transparent,
+                shape = CircleShape),
         contentAlignment = Alignment.Center
     ) {
         Text(
             text = day.date.dayOfMonth.toString(),
             color = when {
-                isMarked && isInMonth -> colorResource(R.color.bronco_950)
-                isInMonth -> colorResource(R.color.bronco_50)
+                isMarked -> colorResource(R.color.bronco_950)
+                isInMonth && !isBeforeEarliestDate  -> colorResource(R.color.bronco_50)
                 else -> colorResource(R.color.bronco_700)
             }
         )
