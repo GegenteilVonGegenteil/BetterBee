443fe69276636283be36af9add220ce1cbcb1d4a
kajak <gegenteilvongegenteil@gmail.com>
2025-01-23 17:38:21 +0100

    Unused import removed

--- app/src/main/java/com/example/betterbe/ui/edit/EditHabitView.kt
+++ app/src/main/java/com/example/betterbe/ui/edit/EditHabitView.kt
@@ -34,7 +34,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
